43bfe423841189900ca486888de788e819246d68
Ron Rise <ron@siteworxpro.com>
2024-09-20 11:25:19 -0400

    Updated golang version

--- Dockerfile
+++ Dockerfile
@@ -9,12 +9,10 @@ ADD . .
 
 RUN go mod tidy && CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on GOOS=linux go build -o /app/aws-iam-anywhere-refresher
 
-FROM ubuntu AS runtime
+FROM alpine:3 AS runtime
 
 WORKDIR /app
 
 COPY --from=build /app/aws-iam-anywhere-refresher aws-iam-anywhere-refresher
-
-RUN apt update && apt install -yqq ca-certificates
 
 ENTRYPOINT ["/app/aws-iam-anywhere-refresher"]
--- main.go
+++ main.go
@@ -6,28 +6,39 @@ import (
 	"git.s.int/rrise/aws-iam-anywhere-refresher/cmd"
 	appConfig "git.s.int/rrise/aws-iam-anywhere-refresher/config"
 	"git.s.int/rrise/aws-iam-anywhere-refresher/kube_client"
-	"log"
+	"github.com/charmbracelet/log"
 	"os"
+	"time"
 )
 
 func main() {
-	println("Starting credentials refresh")
+
+	l := log.NewWithOptions(os.Stderr, log.Options{
+		Level:           log.DebugLevel,
+		ReportTimestamp: true,
+		TimeFormat:      time.RFC3339,
+	})
+	l.Info("Starting credentials refresh")
 
 	client, err := kube_client.NewKubeClient()
 	if err != nil {
-		panic(err)
+		l.Error("Failed to create kubernetes client", "error", err)
+
+		os.Exit(1)
 	}
 
 	c := appConfig.NewConfig()
 
 	privateKey, err := base64.StdEncoding.DecodeString(c.PrivateKey())
 	if err != nil {
-		log.Fatal("error:", err)
+		l.Error("Failed to decode private key", "error", err)
+		os.Exit(1)
 	}
 
 	certificate, err := base64.StdEncoding.DecodeString(c.Certificate())
 	if err != nil {
-		log.Fatal("error:", err)
+		l.Error("Failed to decode certificate", "error", err)
+		os.Exit(1)
 	}
 
 	credentials, err := cmd.Run(&helper.CredentialsOpts{
@@ -43,44 +54,49 @@ func main() {
 	})
 
 	if err != nil {
-		panic(err)
+		l.Error("Failed to refresh credentials", "error", err)
+
+		os.Exit(3)
 	}
 
-	println("Got new credentials")
+	l.Info("Credentials refreshed")
 
 	_, err = client.GetSecret(c.Namespace(), c.Secret())
 	if err != nil {
-		println(err.Error())
-		println("secret doesn't exist, trying to create")
+		l.Error("Failed to get secret", "error", err)
+		l.Info("secret doesn't exist, trying to create")
 		create, err := client.CreateSecret(c.Namespace(), credentials.ToSecret(c.Secret()))
 		if err != nil {
-			panic(err)
+			l.Error("Failed to create secret", "error", err)
+
+			os.Exit(1)
 		}
-		println("secret created")
-		println(create.CreationTimestamp.String())
+		l.Info("Created secret", "created-time-stamp", create.CreationTimestamp.String())
 	} else {
 		update, err := client.UpdateSecret(c.Namespace(), credentials.ToSecret(c.Secret()))
 		if err != nil {
-			panic(err)
+			l.Error("Failed to update secret", "error", err)
+			os.Exit(1)
 		}
-		println("secret updated")
-		println(update.CreationTimestamp.String())
+		l.Info("Updated secret", "updated-time-stamp", update.CreationTimestamp.String())
 	}
 
 	if c.RestartDeployments() {
-		println("Restarting deployments...")
+		l.Info("Restarting deployments")
 		deployments, err := client.ListDeployments(c.Namespace())
 		if err != nil {
-			panic(err)
+			l.Error("Failed to list deployments", "error", err)
+			os.Exit(1)
 		}
 
 		err = client.RestartDeployments(c.Namespace(), deployments)
 		if err != nil {
-			panic(err)
+			l.Error("Failed to restart deployments", "error", err)
+			os.Exit(1)
 		}
 	}
 
-	println("Done!")
+	l.Info("Done!")
 
 	os.Exit(0)
 }
